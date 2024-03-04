0c7f6dfe162cec6f5a5e2718cd8f61360db4b5ce
Sergio <50914599+ImSergioh@users.noreply.github.com>
2024-03-04 08:46:04 +0100

    LanguageMessagesHolder: time alive implementation

--- common/src/main/java/me/imsergioh/pluginsapi/language/LanguageHolder.java
+++ common/src/main/java/me/imsergioh/pluginsapi/language/LanguageHolder.java
@@ -3,7 +3,8 @@ package me.imsergioh.pluginsapi.language;
 import lombok.Getter;
 
 import java.io.File;
-import java.util.HashMap;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class LanguageHolder {
 
@@ -13,22 +14,41 @@ public class LanguageHolder {
     @Getter
     private final Language language;
 
-    private final HashMap<String, LanguageMessagesHolder> messagesHolders = new HashMap<>();
+    private final Map<String, LanguageMessagesHolder> messagesHolders = new HashMap<>();
+    private final Timer timer = new Timer();
 
     public LanguageHolder(Language language) {
         this.language = language;
         this.dir = new File(FOLDER_PATH + "/" + language.name().toUpperCase());
         if (!dir.exists()) dir.mkdirs();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                long currentMillis = System.currentTimeMillis();
+                List<String> toRemove = new ArrayList<>();
+                for (String holderName : messagesHolders.keySet()) {
+                    LanguageMessagesHolder holder = messagesHolders.get(holderName);
+                    long difference = currentMillis - holder.getExpirationMillis();
+                    if (difference > LanguageMessagesHolder.CLEAR_MESSAGE_HOLDER_AFTER) {
+                        toRemove.add(holderName);
+                    }
+                }
+                for (String name : toRemove) {
+                    messagesHolders.remove(name);
+                }
+            }
+        }, LanguageMessagesHolder.CLEAR_MESSAGE_HOLDER_AFTER, LanguageMessagesHolder.CLEAR_MESSAGE_HOLDER_AFTER);
     }
 
     public LanguageMessagesHolder register(String name) {
-        LanguageMessagesHolder holder = new LanguageMessagesHolder(name, this);
-        messagesHolders.put(name, holder);
-        return holder;
+        return messagesHolders.put(name, new LanguageMessagesHolder(name, this));
     }
 
     public LanguageMessagesHolder get(String name) {
         if (!messagesHolders.containsKey(name)) return register(name);
-        return messagesHolders.get(name);
+        LanguageMessagesHolder holder = messagesHolders.get(name);
+        holder.registerUsage();
+        return holder;
     }
 }
--- common/src/main/java/me/imsergioh/pluginsapi/language/LanguageMessagesHolder.java
+++ common/src/main/java/me/imsergioh/pluginsapi/language/LanguageMessagesHolder.java
@@ -6,12 +6,23 @@ import me.imsergioh.pluginsapi.instance.FilePluginConfig;
 @Getter
 public class LanguageMessagesHolder extends FilePluginConfig {
 
+    // Milliseconds:
+    public static final long CLEAR_MESSAGE_HOLDER_AFTER = 1000 * 60;
+
     private final String name;
+
+    @Getter
+    private long expirationMillis;
 
     public LanguageMessagesHolder(String name, LanguageHolder holder) {
         super(holder.getDir() + "/" + name + ".json");
         this.name = name;
+        registerUsage();
         load();
+    }
+
+    public void registerUsage() {
+        this.expirationMillis = System.currentTimeMillis() + CLEAR_MESSAGE_HOLDER_AFTER;
     }
 
 }
