a2f85bf01726147165187393e334a080115c2d12
Sergio <50914599+ImSergioh@users.noreply.github.com>
2024-06-18 22:33:12 +0200

    cambiois

--- velocity-core/src/main/java/me/imsergioh/pluginsapi/instance/backend/listener/ServerRedirectionsListener.java
+++ velocity-core/src/main/java/me/imsergioh/pluginsapi/instance/backend/listener/ServerRedirectionsListener.java
@@ -3,11 +3,9 @@ package me.imsergioh.pluginsapi.instance.backend.listener;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import me.imsergioh.pluginsapi.handler.PubSubConnectionHandler;
-import me.imsergioh.pluginsapi.instance.backend.PlayerServerConnectionsHandler;
 import me.imsergioh.pluginsapi.instance.backend.request.ServerRedirectionRequest;
 import me.imsergioh.pluginsapi.instance.handler.RedisPubSubListener;
 import me.imsergioh.pluginsapi.manager.VelocityPluginsAPI;
-import me.imsergioh.pluginsapi.util.ChatUtil;
 import me.imsergioh.pluginsapi.util.VelocityChatUtil;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -45,10 +43,6 @@ public class ServerRedirectionsListener extends RedisPubSubListener {
 
         // Random selection of server by list:
         RegisteredServer target = list.get(new Random().nextInt(list.size()));
-        if (target != null) {
-            PlayerServerConnectionsHandler.get(player).sendConnectionQueue(target);
-        } else {
-            player.sendMessage(Component.text(NamedTextColor.RED + "Servers not found."));
-        }
+        player.createConnectionRequest(target).fireAndForget();
     }
 }
--- velocity-core/src/main/java/me/imsergioh/pluginsapi/manager/VelocityPluginsAPI.java
+++ velocity-core/src/main/java/me/imsergioh/pluginsapi/manager/VelocityPluginsAPI.java
@@ -1,7 +1,6 @@
 package me.imsergioh.pluginsapi.manager;
 
 import com.velocitypowered.api.proxy.ProxyServer;
-import me.imsergioh.pluginsapi.instance.backend.listener.PlayerServerConnectionsListeners;
 import me.imsergioh.pluginsapi.instance.backend.listener.ServerRedirectionsListener;
 
 public class VelocityPluginsAPI {
@@ -11,7 +10,6 @@ public class VelocityPluginsAPI {
     public static void setup(Object plugin, ProxyServer server) {
         proxy = server;
         ServerRedirectionsListener.register();
-        proxy.getEventManager().register(plugin, new PlayerServerConnectionsListeners());
     }
 
 }
